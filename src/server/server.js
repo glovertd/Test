317892bf81dbd10bb1e1677efacff832d3ddba2b
glovertd <glovertd@gmail.com>
2016-06-07 18:33:38 -0400

    Added bootsrap and mongoose to the project

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,7 +16,22 @@ app.use(express.static('../'));
 // });
 app.use('/*', express.static('../client/index.html'));
 
+mongoose.connect('mongodb://localhost/sample');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error..'));
+db.once('open', function callback(){
+    console.log('sample db opened');
+});
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc){
+    mongoMessage = messageDoc.message;
+});
 
+app.get('*', function(req, res){
+    
+});
 
 app.listen(3000, function () {
     console.log('Express server listening on port 3000');
